e1e6d0529431c458fcb8b7b30171296457bd5d7d
tech-gunk <88415018+tech-gunk@users.noreply.github.com>
2021-12-12 11:20:38 +0530

    Step 2: Frontend of Feed

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,36 +1,59 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RFValue } from "react-native-responsive-fontsize";
+
 import Feed from "../screens/Feed";
 import CreatePost from "../screens/CreatePost";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
         <Tab.Navigator
+            labeled={false}
+            barStyle={styles.bottomTabStyle}
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
-                    if (route.name === 'Feed') {
-                        iconName = focused
-                            ? 'book'
-                            : 'book-outline';
-                    } else if (route.name === 'CreatePost') {
-                        iconName = focused ? 'create' : 'create-outline';
+                    if (route.name === "Feed") {
+                        iconName = focused ? "home" : "home-outline";
+                    } else if (route.name === "CreatePost") {
+                        iconName = focused ? "add-circle" : "add-circle-outline";
                     }
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            size={RFValue(25)}
+                            color={color}
+                            style={styles.icons}
+                        />
+                    );
+                }
             })}
-            tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-            }}
+            activeColor={"#b15487"}
+            inactiveColor={"gray"}
         >
             <Tab.Screen name="Feed" component={Feed} />
             <Tab.Screen name="CreatePost" component={CreatePost} />
         </Tab.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    bottomTabStyle: {
+        backgroundColor: "white",
+        height: "8%",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        overflow: "hidden",
+        position: "absolute"
+    },
+    icons: {
+        width: RFValue(30),
+        height: RFValue(30)
+    }
+});
 
 export default BottomTabNavigator
